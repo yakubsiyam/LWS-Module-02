132adb402e1adf3986a92ca9127983f022baf97b
yakubsiyam <itsyakubsiyam@gmail.com>
2023-02-27 18:51:09 +0600

    Adding LWS module-02

---
+++ src/redux/counter/actions.js
@@ -0,0 +1,13 @@
+import {DECREMENT, INCREMENT} from './actionTypes';
+
+export const increment = () => {
+    return {
+        type: INCREMENT,
+    }
+}
+
+export const decrement = () => {
+    return {
+        type: DECREMENT,
+    }
+}
---
+++ src/redux/rootReducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+import dynamicCounterReducer from "./dynamicCounter/dynamicCounterReducer";
+import counterReducer from "./counter/counterReducer";
+
+const rootReducer = combineReducers({
+    counter: counterReducer,
+    dynamicCounter: dynamicCounterReducer,
+});
+
+export default rootReducer;
