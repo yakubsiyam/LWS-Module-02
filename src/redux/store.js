564097b290e9834fa9e7335b743de83fa6ff075d
Siyam <itsyakubsiyam@gmail.com>
2023-02-27 22:07:17 +0600

    Adding middleware and redux devtool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
+import myLogger from "./middlewares/myLogger";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-// create our first middleware
-const myLogger = (store) => (next) => (action) => {
-    console.log(`Action: ${JSON.stringify(action)}`);
-    console.log(`Before: ${JSON.stringify(store.getState())}`);
-
-    const upcomingState = [action].reduce(rootReducer, store.getState());
-
-    console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
-
-    // pass action
-    return next(action);
-}
-
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, myLogger))
+);
 
 export default store;
